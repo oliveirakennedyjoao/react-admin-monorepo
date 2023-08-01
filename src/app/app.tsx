53240ccf3bef2b09daafccaed898863790009c69
oliveiraKennedyJoao <joaokennedyoliveira@gmail.com>
2023-08-01 15:39:11 -0300

    feat: added comments resource root app.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,23 +1,63 @@
-import { Route, Routes } from 'react-router-dom';
-import { ProductList } from '@myreactapp/modules/products';
-import { OrderList } from '@myreactapp/modules/orders';
-import { Posts } from '@myreactapp/posts';
+// import { Route, Routes } from 'react-router-dom';
+// import { ProductList } from '@myreactapp/modules/products';
+// import { OrderList } from '@myreactapp/modules/orders';
+// import { Posts } from '@myreactapp/posts';
+// import { Comments } from '@myreactapp/comments';
+
+import { Admin, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+import { UserList } from './users';
+import { PostList } from './posts/list';
+import { PostEdit } from './posts/edit';
+import { PostCreate } from './posts/create';
+
+import PostIcon from '@mui/icons-material/Book';
+import UserIcon from '@mui/icons-material/Group';
+import { Dashboard } from './dashboard';
+import { authProvider } from './providers/auth';
 import { Comments } from '@myreactapp/comments';
 
-function Home() {
-  return <h1>Welcome to the App</h1>;
-}
+const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-export function App() {
+export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/products" element={<ProductList />}></Route>
-      <Route path="/orders" element={<OrderList />}></Route>
-      <Route path="/posts" element={<Posts />}></Route>
-      <Route path="/comments" element={<Comments />}></Route>
-    </Routes>
+    <Admin
+      authProvider={authProvider}
+      dataProvider={dataProvider}
+      dashboard={Dashboard}
+    >
+      <Resource
+        name="users"
+        list={UserList}
+        icon={UserIcon}
+        recordRepresentation="name"
+      />
+      <Resource
+        name="posts"
+        icon={PostIcon}
+        list={PostList}
+        edit={PostEdit}
+        create={PostCreate}
+      />
+      <Resource name="comments" list={Comments} />
+    </Admin>
   );
 }
 
-export default App;
+// function Home() {
+//   return <h1>Welcome to the App</h1>;
+// }
+
+// export function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Home />}></Route>
+//       <Route path="/products" element={<ProductList />}></Route>
+//       <Route path="/orders" element={<OrderList />}></Route>
+//       <Route path="/posts" element={<Posts />}></Route>
+//       <Route path="/comments" element={<Comments />}></Route>
+//     </Routes>
+//   );
+// }
+
+// export default App;
---
+++ src/app/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+
+export function Dashboard() {
+  return (
+    <Card>
+      <CardHeader title="Welcome to the administration" />
+      <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+    </Card>
+  );
+}
