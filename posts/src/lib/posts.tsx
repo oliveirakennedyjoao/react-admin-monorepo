1d9e6e4321bd4a42483e32128b32936e08d50e94
oliveiraKennedyJoao <joaokennedyoliveira@gmail.com>
2023-08-04 00:55:55 -0300

    feat(posts): adicionado subtitulo ao componente.

--- posts/src/lib/posts.tsx
+++ posts/src/lib/posts.tsx
@@ -7,6 +7,7 @@ export function Posts(props: PostsProps) {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Posts!</h1>
+      <h3>Third subtitle.</h3>
     </div>
   );
 }
