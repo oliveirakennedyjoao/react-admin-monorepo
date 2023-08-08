162a7d1f295f74a56ed678016087d223fa54cd90
oliveiraKennedyJoao <joaokennedyoliveira@gmail.com>
2023-08-08 13:30:18 -0300

    feat: added 3rd subtitle to other page.

--- other/src/lib/list/index.tsx
+++ other/src/lib/list/index.tsx
@@ -3,6 +3,7 @@ export function OtherList() {
     <>
       <h1> Other list works!</h1>
       <h2>And renders another subtitle.</h2>
+      <h3>Now it renders the third subtitle.</h3>
     </>
   );
 }
