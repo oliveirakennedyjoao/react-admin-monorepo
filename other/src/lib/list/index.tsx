766151fabaa085b681996e869e3b5b484189e771
oliveiraKennedyJoao <joaokennedyoliveira@gmail.com>
2023-08-10 17:34:18 -0300

    chore: adding import constraints using tags.

--- other/src/lib/list/index.tsx
+++ other/src/lib/list/index.tsx
@@ -1,9 +1,12 @@
+// import { Button } from '@myreactapp/shared/ui';
+
 export function OtherList() {
   return (
     <>
       <h1> Other list works!</h1>
       <h2>And renders another subtitle.</h2>
       <h3>Now it renders the third subtitle.</h3>
+      {/* <Button /> */}
     </>
   );
 }
